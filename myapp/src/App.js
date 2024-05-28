eb87d39afb47846a03c81cf945385db44b96d5cc
ben lim <blim0e89@gmail.com>
2024-05-28 20:21:45 +0800

    add frontend

---
+++ myapp/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App-header">
+        <div className="left">
+          <div id="start">
+            HowToGraduate
+          </div>
+        </div>
+        
+        <button id="right">
+          Login
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
